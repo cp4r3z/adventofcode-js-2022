d31f1e538d729a78e062178024761585e6c6e22e
cp4r3z <cp4r3z@gmail.com>
2023-08-28 19:59:18 -0500

    comparison begin

--- 13/aoc.ts
+++ 13/aoc.ts
@@ -1,27 +1,3 @@
-// class Elf {
-//     readonly calories: number;
-
-//     constructor(calorieArray: Array<string>) {
-//         this.calories = calorieArray
-//             .map(s => parseInt(s))
-//             .reduce((prev, cur) => cur + prev, 0);
-//     }
-// }
-
-// const solve = (input: string): Array<Elf> => {
-//     const sanitized = input.replaceAll('\r', '');
-
-//     const elves: Array<Elf> = input
-//         .replaceAll('\r', '') // Sanitize input
-//         .split('\n\n') // Split into lists of calories
-//         .map(e => e.split('\n')) // Convert lists into arrays
-//         .map(e => new Elf(e)) // Convert into Elf objects
-//         .sort((elfA, elfB) => elfB.calories - elfA.calories) // Descending
-//         .slice(0, 3); // Return top 3
-
-//     return elves;
-// };
-
 const splitter = (list: string): string[] => {
     if (list.charAt(0) !== '[') {
         throw new Error('Invalid list: does not start with [');
@@ -49,6 +25,12 @@ const splitter = (list: string): string[] => {
     outArray.push(partialList); // push last partial
     return outArray;
 };
+
+enum Comparison {
+    Less,
+    Equal,
+    Greater
+}
 
 class Item {
     readonly value?: Number;
@@ -96,13 +78,83 @@ class Item {
             this.children.push(new Item(partial));
         }
     }
+
+    Compare(other: Item): Comparison {
+
+        if (this.value && other.value) {
+            if (this.value > other.value) {
+                return Comparison.Greater;
+            }
+            else if (this.value < other.value) {
+                return Comparison.Less;
+            }
+            else {
+                return Comparison.Equal;
+            }
+        }
+
+        let thisItem: Item = this;
+        let otherItem: Item = other;
+
+        if (this.value) {
+            thisItem = new Item(`[${this.value}]`);
+        }
+        if (other.value) {
+            otherItem = new Item(`[${other.value}]`);
+        }
+
+        // So I think this is kinda stupid... Remove outside brackets, split(','), repeat
+
+        let index = 0;
+
+        while (thisItem.children[index] && otherItem.children[index]) {
+            const comparison = thisItem.children[index].Compare(otherItem.children[index]);
+            if (comparison !== Comparison.Equal) {
+                return comparison;
+            }
+            index++;
+        }
+
+        if (!thisItem.children[index]) { return Comparison.Less; }
+        if (!otherItem.children[index]) { return Comparison.Greater; }
+
+        return Comparison.Equal;
+
+        throw new Error('should not get here');
+    }
+}
+
+const solve = (input: string): Number => {
+
+    const pairs = input
+        .trim()
+        .replaceAll('\r', '') // Sanitize input
+        .split('\n\n') // Split into lists of calories
+        .map(e => e.split('\n')) // Convert lists into arrays
+
+    pairs.forEach((pair, index) => {
+        const item0 = new Item(pair[0]);
+        const item1 = new Item(pair[1]);
+
+        const comparison = item0.Compare(item1);
+        const isRightOrder = comparison === Comparison.Less;
+        console.log(isRightOrder);
+    });
+    return 0;
+
+
 }
 
 const part1 = (input: string) => {
-    const test = splitter(input);
-    const test2 = new Item(input);
-    console.log('blah');
+
+
+    const result = solve(input);
+
+    //const test = splitter(input);
+    //const test2 = new Item(input);
+    //console.log('blah');
+    return result;
 } //solve(input)[0].calories;
 const part2 = (input: string) => { }// solve(input).reduce((prev, cur) => prev + cur.calories, 0);
 
-export { part1, part2, splitter };
+export { part1, part2 };
