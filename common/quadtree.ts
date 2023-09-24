736d54b333c17c4b283c206da79a47f45e56c903
cp4r3z <cp4r3z@gmail.com>
2023-09-24 08:17:57 -0500

    Fixes and cleanup for QuadTree but we cannot use it

--- common/quadtree.ts
+++ common/quadtree.ts
@@ -61,93 +61,6 @@ export class Rectangle extends Shape.Rectangle {
     }
 }
 
-// Rectangle
-// class BoundsOld {
-
-//     readonly x0y0: Point.XY;
-//     readonly x1y1: Point.XY;
-
-//     constructor(x0y0: Point.XY, x1y1: Point.XY) {
-//         // TODO: Sanitize Bounds, maybe create a SquareBase2 subclass?
-
-//         this.x0y0 = Point.XY.Copy(x0y0);
-//         this.x1y1 = Point.XY.Copy(x1y1);
-//     }
-
-//     /**
-//      * @param delta Shift while copying
-//      */
-//     copy = (delta?: Point.XY) => {
-//         let x0y0 = Point.XY.Copy(this.x0y0);
-//         let x1y1 = Point.XY.Copy(this.x1y1);
-//         if (delta) {
-//             x0y0.x += delta.x;
-//             x1y1.x += delta.x;
-//             x0y0.y += delta.y;
-//             x1y1.y += delta.y;
-//         }
-//         return new Bounds(x0y0, x1y1);
-//     }
-
-//     //TODO: Unit test this guy
-//     intersects = (other: Bounds): boolean => {
-//         const rangeX = [[this.x0y0.x, this.x1y1.x], [other.x0y0.x, other.x1y1.x]];
-//         const rangeY = [[this.x0y0.y, this.x1y1.y], [other.x0y0.y, other.x1y1.y]];
-
-//         // TODO: Too confusing?
-//         return [rangeX, rangeY].reduce((prev, cur) => {
-//             if (!prev) return false;
-//             // Sort each range pair based on the lowest value (x0y0)
-//             const sorted = cur.sort((a, b) => a[0] - b[0]);
-//             // If the lowest value of the "high" is less than the highest value of the "low", it intersects
-//             // "On edge" counts
-//             const result = sorted[1][0] <= sorted[0][1];
-//             return result;
-//         }, true);
-//     }
-
-//     //TODO: Unit test this guy
-//     contains = (other: Bounds): boolean => {
-//         const rangeX = [[this.x0y0.x, this.x1y1.x], [other.x0y0.x, other.x1y1.x]];
-//         const rangeY = [[this.x0y0.y, this.x1y1.y], [other.x0y0.y, other.x1y1.y]];
-
-//         // TODO: Too confusing
-//         return [rangeX, rangeY].reduce((prev, cur) => {
-//             if (!prev) return false;
-//             // cur[0] this
-//             // cur[1] other
-//             // this's low is lower (or equal) than other's low and this's high is higher than (or equal) other's high
-//             const result = cur[0][0] <= cur[1][0] && cur[0][1] >= cur[1][1];
-//             return result;
-//         }, true);
-//     }
-
-//     // Approximate center (rounded to next integer)
-//     center = (): Point.XY => {
-//         const dimX = this.x1y1.x - this.x0y0.x;
-//         const dimY = this.x1y1.y - this.x0y0.y;
-//         const center = new Point.XY(Math.ceil(dimX / 2), Math.ceil(dimY / 2));
-//         return center;
-//     }
-
-//     hasPoint.XY = (coor: Point.XY): Boolean => {
-//         return (
-//             coor.x >= this.x0y0.x && coor.x <= this.x1y1.x &&
-//             coor.y >= this.x0y0.y && coor.y <= this.x1y1.y
-//         );
-//     }
-
-//     isPoint.XY = (coor: Point.XY): Boolean => {
-//         return (
-//             coor.x === this.x0y0.x && coor.x === this.x1y1.x &&
-//             coor.y === this.x0y0.y && coor.y === this.x1y1.y
-//         );
-//     }
-
-// };
-
-// Consider also a data structure that holds a quadtree, but can resize the root!
-
 export enum ActiveState {
     INACTIVE,
     ACTIVE,
@@ -191,19 +104,9 @@ export class QuadTree<T> {
     constructor(bounds: Rectangle) {
 
         this.bounds = bounds.copy();
-
-
-        //this.node = null;
         this.data = null;
         this.active = ActiveState.ACTIVE;
         this.quads = null;
-
-        // {
-        //     x0y0: null,
-        //     x0y1: null,
-        //     x1y0: null,
-        //     x1y1: null
-        // }
     }
 
     area = (): number => this.bounds.area(true);
@@ -302,16 +205,9 @@ export class QuadTree<T> {
         if (bounds.contains(this.bounds)) {
             this.data = data;
             // this.active = ActiveState.ACTIVE;
-            //console.log('set!')
 
             // No need to have children anymore
             this.quads = null;
-            //  {
-            //     x0y0: null,
-            //     x0y1: null,
-            //     x1y0: null,
-            //     x1y1: null
-            // };
 
             return;
         }
@@ -322,9 +218,6 @@ export class QuadTree<T> {
 
             // this.active = ActiveState.VARIOUS;
 
-           
-            
-
             if (!this.hasChildren) {
                 const wasSplit = this.split();
                 if (!wasSplit) {
@@ -332,36 +225,32 @@ export class QuadTree<T> {
                 }
             }
 
-            
-
             for (const [key, quad] of Object.entries(this.quads)) {
                 //console.log(`${key}: ${value}`);
                 quad.Set(bounds, data);
             }
 
-            //this.data = null;
+            this.data = null;
 
             // If all children have the same value, set data and remove children
 
             let allSame = true;
             let quadData;
 
-            if (this.quads.x0y0.data !== null){
+            if (this.quads.x0y0.data !== null) {
                 quadData = this.quads.x0y0.data;
                 for (const [key, quad] of Object.entries(this.quads)) {
-                    if (quad.data !== quadData){
+                    if (quad.data !== quadData) {
                         allSame = false;
                         break;
                     }
                 }
-            }   
-            
+            }
+
             if (quadData !== null && quadData !== undefined && allSame) {
                 this.data = quadData;
                 this.quads = null;
             }
-
-            
         }
     }
 
